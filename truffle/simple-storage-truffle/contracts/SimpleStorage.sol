5c9968af633028a4bab9fff4766ec3871cdf8084
sovni <christophe.burgalat@gmail.com>
2021-10-06 22:27:50 +0200

    Add truffle storage sample

---
+++ truffle/simple-storage-truffle/contracts/SimpleStorage.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.6.11;
+
+contract SimpleStorage {
+   uint data;
+
+   function set(uint x) public {
+       data = x;
+   }
+
+   function get() public view returns (uint) {
+       return data;
+   }
+}
